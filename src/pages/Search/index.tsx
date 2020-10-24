f34c74185fc4e73856daaf9a9e1be2a47d9c9079
Vitor Lucas <vitorluc.2013@gmail.com>
2020-10-24 14:58:19 -0300

    visual of the 'search' page, ok

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import './styles.css';
+import Button from '../../core/components/Button';
+import './styles.scss';
 
 const Search = () => (
-    <div>
-        <h1>Search</h1>
+    <div className='search-container'>
+        <div className="search-content">
+            <div className='search-title'>
+                <h5>Encontre um perfil Github</h5>
+            </div>
+            <div className="form-search">
+                <input type='text' placeholder='Usuário Github' className='input-search' />
+            </div>
+            <div className='btn-form'>
+                <Button text='Encontrar' />
+            </div>
+        </div>
     </div>
 );
 
